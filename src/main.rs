9bd9652717f1d7b1e464000ef178887182767b7e
David Beck <david.beck.priv@gmail.com>
2016-11-07 21:24:32 +0000

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 extern crate acto_rs;
 pub mod naked;
 
-fn first() {
-  use acto_rs::scheduler;
-  let mut sched = scheduler::new();
-  sched.start_with_threads(4);
-  sched.stop();
-}
-
 fn main() {
-  first();
-  println!("Hello, world!");
   naked::run_naked();
 }
